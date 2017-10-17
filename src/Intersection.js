82e3f5abcdd7537cf22888b682760305adc813c2
gmachure <gmachure@adexos.fr>
2017-10-17 14:54:29 +0200

    ecriture code story 8 + test validé

--- src/Intersection.js
+++ src/Intersection.js
@@ -4,8 +4,25 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
     var state = Lyngk.State.VACANT;
+    var colorLastPiece;
+    var nb_pieces = 0;
 
-    this.getState = function() {
+    this.getState = function () {
         return state;
+    };
+
+    this.getColorLastPiece = function () {
+      return colorLastPiece;
+    };
+
+    this.poserPiece = function (color) {
+        var piece = new Lyngk.Piece(color);
+        colorLastPiece = piece.getcolor();
+        if (nb_pieces === 0) {
+            state = Lyngk.State.ONE_PIECE;
+        }
+        nb_pieces++;
     }
+
+
 };
--- src/Piece.js
+++ src/Piece.js
@@ -1,4 +1,9 @@
 "use strict";
 
 Lyngk.Piece = function (c) {
+    var color = c;
+
+    this.getcolor = function () {
+        return color;
+    }
 };
